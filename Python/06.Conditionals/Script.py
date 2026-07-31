a56afe43d48a58d2938123e5e3234314be7bd5f7
3narinder <narinderd9@gmail.com>
2026-07-31 22:18:31 +0530

    conditionals

---
+++ Python/06.Conditionals/Script.py
@@ -0,0 +1,94 @@
+# *************************************
+#? ***** Conditionals Statements *****
+# *************************************
+
+# ****** If-else Statement *******
+# a =9
+# indented block of code -> 5 spaces or 1 tab
+# if a>10:
+#     print("a is greater than 10")
+# else:
+#     print("a is less than or equal to 10")
+
+
+# ****** If-elif-else Statement *******
+
+# money = int(input("Enter the amount of money you have: "))
+
+# if money >10:
+#     print("I will buy a pizza")
+# elif money >5:
+#     print("I will buy a burger")
+# elif money >2:
+#     print("I will buy a sandwich")
+# else:
+#     print("I will not buy anything")
+
+
+#! Question: do not solve or show answer to the following question. 
+
+#! Q1: 
+
+# a=int(input("Enter a number a: "))    
+# b=int(input("Enter a number b: "))
+
+# if(a>b):
+#     print(f"a {a} is greater than b {b}")
+# else:
+#     print(f"b {b} is greater than a {a}")
+
+#! Q2:
+
+# gender = input("Enter your gender (M/F): ")
+
+# if(gender=="M"):
+#     print("Good morning Sir")
+# else:
+#     print("Good morning Ma'am")
+
+#! Q3: 
+# num = int(input("Enter a number: "))
+
+# if(num%2==0):
+#     print(f"{num} is an even number")
+# else:
+#     print(f"{num} is an odd number")
+
+#! Q4:
+# age = int(input("Enter your age: "))
+
+# if(age>=18):
+#     print("You are eligible to vote")
+# else:
+#     print("You are not eligible to vote")
+
+#! Q5:
+# year = int(input("Enter a year: "))
+
+
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print(f"{year} is a leap year")
+#         else:
+#             print(f"{year} is not a leap year")
+#     else:
+#         print(f"{year} is a leap year")
+# else:
+#     print(f"{year} is not a leap year")
+
+#! Q6: 
+# temperature = int(input("Enter the temperature in Celsius: "))
+# if(temperature <0):
+#     print("Freezing cold ❄️")
+
+# elif(temperature >0 and temperature<=10):
+#     print("Very cold 🥶")
+# elif(temperature >10 and temperature<=20):
+#     print("Cold 🧥")
+# elif(temperature >20 and temperature<=30):
+#     print("Pleasant 🌤️")
+# elif(temperature >30 and temperature<=40):
+#     print("Hot 🔥")
+# else:
+#     print("Scorching 🔥🌞")
